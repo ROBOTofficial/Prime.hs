45bf180d3d05653614aaa54d46ed8990b5c4f619
ROBOT <tkata12@outlook.jp>
2024-04-28 01:02:34 +0900

    first commit

---
+++ mian.hs
@@ -0,0 +1,32 @@
+import System.IO ()
+
+solve :: Int -> IO ()
+solve n
+    | isPrime n = do
+        appendFile "prime.txt" (show n ++ "\n")
+
+for :: Int -> Int -> IO ()
+for n max
+    | n > max = putStrLn "END"
+    | isPrime n = do
+        appendFile "prime.txt" (show n ++ "\n")
+        for (n + 1) max
+    | otherwise = do
+        for (n + 1) max
+
+isPrime :: Int -> Bool
+isPrime 1 = False
+isPrime 2 = True
+isPrime n = all (isNotDivisible n) [2..end]
+    where 
+        end = floor $ sqrt (fromIntegral n)
+
+isNotDivisible :: Int -> Int -> Bool
+isNotDivisible x y = x `mod` y /= 0
+
+main :: IO ()
+main = do
+    writeFile "prime.txt" ""
+    putStrLn "上限値を設定してください"
+    n <- readLn
+    for 2 n
